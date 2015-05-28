a0b1537957d10b8a1c5f6241b5107a006326b315
loicbourgois <loicbourgois.web@gmail.com>
2015-05-28 22:38:58 +0200

    Divide job creator

--- job-creator.cpp
+++ job-creator.cpp
@@ -2,6 +2,8 @@
 #include "util.hpp"
 #include "database-manager.hpp"
 
+#include <QVector>
+
 JobCreator::JobCreator()
 {
 
@@ -26,32 +28,10 @@ void JobCreator::createJob(const QDate & dateStart,
     {
         for(QDate date = dateStart ; date <= dateEnd ; date = date.addDays(1))
         {
-            // get list races
             foreach (QString completeIdRace, DatabaseManager::getCompleteIdRaces(date))
             {
-                Util::addMessage("Processing race : " + completeIdRace);
-                // get pony infos
-                foreach (QString ponyName, DatabaseManager::getPoniesFromRace(completeIdRace))
-                {
-                    Util::addMessage("  Processing pony : " + ponyName);
-                    int raceCount = DatabaseManager::getPonyRaceCount(ponyName, dateStartHistory, date);
-                    int firstCount = DatabaseManager::getPonyFirstCount(ponyName, dateStartHistory, date);
-                    // pony = {ponyname, count, count};
-                }
-                // get pony infos
-                foreach (QString jockeyName, DatabaseManager::getJockeysFromRace(completeIdRace))
-                {
-                    int raceCount = DatabaseManager::getJockeyRaceCount(jockeyName, dateStartHistory, date);
-                    int firstCount = DatabaseManager::getJockeyFirstCount(jockeyName, dateStartHistory, date);
-                    // pony = {ponyname, count, count};
-                }
-                // get pony infos
-                foreach (QString trainerName, DatabaseManager::getTrainersFromRace(completeIdRace))
-                {
-                    int raceCount = DatabaseManager::getTrainerRaceCount(trainerName, dateStartHistory, date);
-                    int firstCount = DatabaseManager::getTrainerFirstCount(trainerName, dateStartHistory, date);
-                    // pony = {ponyname, count, count};
-                }
+                processRace(completeIdRace, dateStartHistory, date);
+
             }
         }
     }
@@ -64,4 +44,41 @@ void JobCreator::createJob(const QDate & dateStart,
     {
         Util::addError(error);
     }
+}
+
+void JobCreator::processRace(const QString &completeIdRace,
+                             const QDate &dateStartHistory,
+                             const QDate &dateEndHistory)
+{
+    // Init
+    Util::addMessage("Processing race : " + completeIdRace);
+    QVector<Pony> ponies;
+    QVector<Jockey> jockeys;
+    QVector<Trainer> trainers;
+    // Ponies
+    foreach (QString ponyName,
+             DatabaseManager::getPoniesFromRace(completeIdRace))
+    {
+        int raceCount = DatabaseManager::getPonyRaceCount(ponyName, dateStartHistory, dateEndHistory);
+        int firstCount = DatabaseManager::getPonyFirstCount(ponyName, dateStartHistory, dateEndHistory);
+        ponies.push_back(Pony{ponyName, raceCount, firstCount});
+        Util::addMessage("  " + ponyName + " : "
+                         + QString::number(firstCount)
+                         + " / " + QString::number(raceCount));
+    }
+    // Jockeys
+    foreach (QString jockeyName, DatabaseManager::getJockeysFromRace(completeIdRace))
+    {
+        int raceCount = DatabaseManager::getJockeyRaceCount(jockeyName, dateStartHistory, dateEndHistory);
+        int firstCount = DatabaseManager::getJockeyFirstCount(jockeyName, dateStartHistory, dateEndHistory);
+        jockeys.push_back(Jockey{jockeyName, raceCount, firstCount});
+    }
+    // Trainers
+    foreach (QString trainerName, DatabaseManager::getTrainersFromRace(completeIdRace))
+    {
+        int raceCount = DatabaseManager::getTrainerRaceCount(trainerName, dateStartHistory, dateEndHistory);
+        int firstCount = DatabaseManager::getTrainerFirstCount(trainerName, dateStartHistory, dateEndHistory);
+        trainers.push_back(Trainer{trainerName, raceCount, firstCount});
+    }
+    //Util::addMessage("  ponies : " + QString::number(ponies.size()));
 }
--- job-creator.hpp
+++ job-creator.hpp
@@ -5,12 +5,33 @@
 
 class JobCreator
 {
-    public:
-        JobCreator();
-        ~JobCreator();
-        static void createJob(const QDate & dateStart,
-                              const QDate & dateEnd,
-                              const QDate & dateStartHistory);
+    struct Pony
+    {
+        QString name;
+        int raceCount;
+        int firstCount;
+    };
+    struct Jockey
+    {
+        QString name;
+        int raceCount;
+        int firstCount;
+    };
+    struct Trainer
+    {
+        QString name;
+        int raceCount;
+        int firstCount;
+    };
+public:
+    JobCreator();
+    ~JobCreator();
+    static void createJob(const QDate & dateStart,
+                          const QDate & dateEnd,
+                          const QDate & dateStartHistory);
+    static void processRace(const QString & completeIdRace,
+                            const QDate &  dateStartHistory,
+                            const QDate &  dateEndHistory);
 };
 
 #endif // JOBCREATOR_HPP
