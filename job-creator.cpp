d67ff56f4565a953b6ba1f9f8104380f838da790
Loïc Bourgois <loicbourgois.web@gmail.com>
2015-05-28 15:50:35 +0200

    Init job creator

--- job-creator.cpp
+++ job-creator.cpp
@@ -1,4 +1,5 @@
 #include "job-creator.hpp"
+#include "util.hpp"
 
 JobCreator::JobCreator()
 {
@@ -10,3 +11,16 @@ JobCreator::~JobCreator()
 
 }
 
+void JobCreator::createJob(const QDate & dateStart,
+                           const QDate & dateEnd,
+                           const QDate & dateStartHistory)
+{
+    Util::addMessage("Creating job");
+    // Init
+    bool ok = true;
+    QString error = "";
+    //
+
+    //
+
+}
--- job-creator.hpp
+++ job-creator.hpp
@@ -1,12 +1,16 @@
 #ifndef JOBCREATOR_HPP
 #define JOBCREATOR_HPP
 
+#include <QDate>
 
 class JobCreator
 {
     public:
         JobCreator();
         ~JobCreator();
+        static void createJob(const QDate & dateStart,
+                              const QDate & dateEnd,
+                              const QDate & dateStartHistory);
 };
 
 #endif // JOBCREATOR_HPP
